dc3168388ff79017968eac70b96ff0ebf1d21724
Timofey Dergachev <t@exeto.me>
2020-04-28 13:28:03 +0300

    Add tests for set util

--- src/utils.ts
+++ src/utils.ts
@@ -2,19 +2,28 @@ import { Errors, Touched } from './types';
 
 const isNull = (value: any) => value === null;
 
-const isObject = (value: any): value is object =>
+const isObject = (value: any): value is { [key: string]: any } =>
   !isNull(value) && typeof value === 'object';
 
 const isNumber = (value: any): value is number => typeof value === 'number';
 
-const parseKey = (key: string) => {
-  const int = Number(key);
+const isString = (value: any): value is string => typeof value === 'string';
 
-  return Number.isNaN(int) ? key : int;
-};
+const parseName = (name: string) =>
+  name.split('.').reduce((acc: (string | number)[], key) => {
+    const match = key.match(/(.+)\[(\d+)\]$/);
+
+    if (match) {
+      acc.push(match[1], Number(match[2]));
+    } else {
+      acc.push(key);
+    }
+
+    return acc;
+  }, []);
 
 export const get = <Values>(values: Values, name: string) => {
-  const getInner = (keys: string[], valuesInner: any): any => {
+  const getInner = (keys: (string | number)[], valuesInner: any): any => {
     if (!keys.length) {
       return valuesInner;
     }
@@ -29,7 +38,7 @@ export const get = <Values>(values: Values, name: string) => {
     return restKeys.length ? undefined : value;
   };
 
-  return getInner(name.split('.'), values);
+  return getInner(parseName(name), values);
 };
 
 export const set = <Values>(
@@ -37,43 +46,40 @@ export const set = <Values>(
   name: string,
   value: any,
 ): Values => {
-  const setInner = (keys: string[], valuesInner: any): any => {
+  const setInner = (keys: (string | number)[], valuesInner: any): any => {
     if (!keys.length) {
       return value;
     }
 
     const [key, ...restKeys] = keys;
-    const valueInner = valuesInner[key];
 
-    if (isObject(valuesInner)) {
+    if (!isObject(valuesInner) && !Array.isArray(valuesInner)) {
+      valuesInner = isNumber(key) ? [] : {};
+    }
+
+    if (isString(key) && isObject(valuesInner)) {
       return {
         ...valuesInner,
-        [key]: setInner(restKeys, valueInner),
+        [key]: setInner(restKeys, valuesInner[key]),
       };
     }
 
-    if (Array.isArray(valuesInner)) {
-      const copy = valuesInner.slice();
+    const arr = valuesInner.slice();
 
-      copy[Number(key)] = setInner(restKeys, valueInner);
+    arr[key] = setInner(restKeys, valuesInner[key]);
 
-      return copy;
-    }
-
-    const empty = isNumber(parseKey(key)) ? [] : {};
-
-    return setInner(restKeys, empty);
+    return arr;
   };
 
-  return setInner(name.split('.'), values);
+  return setInner(parseName(name), values);
 };
 
 export const getTouched = (errors: Errors) =>
-  Object.keys(errors).reduce((acc, key) => {
+  Object.keys(errors).reduce((acc: Touched, key) => {
     acc[key] = true;
 
     return acc;
-  }, {} as Touched);
+  }, {});
 
 export const omitOne = <T extends any>(values: T, key: string): T => {
   const copy = { ...values };
--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,24 +1,24 @@
-import { get } from '../src/utils';
+import { get, set } from '../src/utils';
+
+const values = {
+  id: 42,
+  info: {
+    name: 'John',
+  },
+  commentsIds: [56, 78],
+  comments: [
+    {
+      id: 56,
+      text: 'foo',
+    },
+    {
+      id: 78,
+      text: 'bar',
+    },
+  ],
+};
 
 describe('get', () => {
-  const values = {
-    id: 42,
-    info: {
-      name: 'John',
-    },
-    commentsIds: [56, 78],
-    comments: [
-      {
-        id: 56,
-        text: 'foo',
-      },
-      {
-        id: 78,
-        text: 'bar',
-      },
-    ],
-  };
-
   it('should return value', () => {
     expect(get(values, 'id')).toBe(values.id);
   });
@@ -28,23 +28,43 @@ describe('get', () => {
   });
 
   it('should return value from array', () => {
-    expect(get(values, 'commentsIds.0')).toBe(values.commentsIds[0]);
-    expect(get(values, 'commentsIds.1')).toBe(values.commentsIds[1]);
+    expect(get(values, 'commentsIds[0]')).toBe(values.commentsIds[0]);
+    expect(get(values, 'commentsIds[1]')).toBe(values.commentsIds[1]);
     expect(get(values, 'comments')).toBe(values.comments);
-    expect(get(values, 'comments.0.text')).toBe(values.comments[0].text);
-    expect(get(values, 'comments.1.text')).toBe(values.comments[1].text);
+    expect(get(values, 'comments[0].text')).toBe(values.comments[0].text);
+    expect(get(values, 'comments[1].text')).toBe(values.comments[1].text);
   });
 
   it('should return undefined when incorrect path', () => {
     expect(get(values, 'incorrect')).toBeUndefined();
     expect(get(values, 'incorrect.incorrect')).toBeUndefined();
-    expect(get(values, 'incorrect.0.incorrect')).toBeUndefined();
+    expect(get(values, 'incorrect[0].incorrect')).toBeUndefined();
     expect(get(values, 'id.incorrect')).toBeUndefined();
     expect(get(values, 'info.incorrect')).toBeUndefined();
-    expect(get(values, 'commentsIds.3')).toBeUndefined();
-    expect(get(values, 'commentsIds.3.incorrect')).toBeUndefined();
-    expect(get(values, 'comments.3')).toBeUndefined();
-    expect(get(values, 'comments.2.incorrect')).toBeUndefined();
-    expect(get(values, 'comments.3.incorrect')).toBeUndefined();
+    expect(get(values, 'commentsIds[3]')).toBeUndefined();
+    expect(get(values, 'commentsIds[3].incorrect')).toBeUndefined();
+    expect(get(values, 'comments[3]')).toBeUndefined();
+    expect(get(values, 'comments[2].incorrect')).toBeUndefined();
+    expect(get(values, 'comments[3].incorrect')).toBeUndefined();
+  });
+});
+
+describe('set', () => {
+  it('should set value', () => {
+    expect(set(values, 'id', 84)).toMatchSnapshot();
+  });
+
+  it('should set value to nested path', () => {
+    expect(set(values, 'info.name', 'Mike')).toMatchSnapshot();
+  });
+
+  it('should set value to array', () => {
+    expect(set(values, 'comments[0].text', 'baz')).toMatchSnapshot();
+    expect(set(values, 'comments[1].text', 'bat')).toMatchSnapshot();
+  });
+
+  it('should set non-existent path', () => {
+    expect(set({}, 'foo[0].bar', 'baz')).toMatchSnapshot();
+    expect(set({ foo: 42 }, 'foo[0].bar.baz', 'bat')).toMatchSnapshot();
   });
 });
